9de738e84508677eb2385dfe80324cb46e7236c0
Brian Christian <brchristian@gmail.com>
2017-03-13 18:15:55 -0700

    whitespace

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -3,7 +3,7 @@ Spree::Order.class_eval do
 
   def capture_tax_cloud
     return unless is_taxed_using_tax_cloud?
-    response =  Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
+    response = Spree::TaxCloud.transaction_from_order(self).authorized_with_capture
     if response != "OK"
       Rails.logger.error "ERROR: TaxCloud returned an order capture response of #{response}."
     end
