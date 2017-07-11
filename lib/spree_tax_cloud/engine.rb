8353cadf489d6a69a8efbc5f8af3ab58830e05ab
Brian Christian <brchristian@gmail.com>
2017-07-11 15:35:17 -0700

    distinguish frontend and backend dependencies

--- lib/spree_tax_cloud/engine.rb
+++ lib/spree_tax_cloud/engine.rb
@@ -20,15 +20,29 @@ module SpreeTaxCloud
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
 
+      if SpreeTaxCloud::Engine.backend_available?
+        Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/admin/*_decorator*.rb")) do |c|
+          Rails.configuration.cache_classes ? require(c) : load(c)
+        end
+      end
+      
       if SpreeTaxCloud::Engine.frontend_available?
         Rails.application.config.assets.precompile += [
           'lib/assets/javascripts/spree/frontend/spree_tax_cloud.js',
           'lib/assets/stylesheets/spree/frontend/spree_tax_cloud.css'
         ]
-        Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/frontend/*_decorator*.rb")) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
       end
+    end
+
+    def self.backend_available?
+      @@frontend_available ||= ::Rails::Engine.subclasses.map(&:instance).map { |e| e.class.to_s }.include?('Spree::Backend::Engine')
+    end
+
+    if self.backend_available?
+      paths["app/controllers"] << "lib/controllers/admin"
     end
 
     def self.frontend_available?
