1be58f32297ee49b8db52f6df223212b84e8b9a9
Brian Christian <brchristian@gmail.com>
2018-01-29 15:53:17 -0800

    add original hash to deface override

--- app/overrides/spree/admin/products/_form.rb
+++ app/overrides/spree/admin/products/_form.rb
@@ -2,5 +2,6 @@ Deface::Override.new(
   virtual_path: 'spree/admin/products/_form',
   name: 'add_tic_to_admin_product_edit',
   insert_after: "[data-hook='admin_product_form_tax_category']",
-  partial: 'spree/admin/products/edit_tax_cloud_tic'
+  partial: 'spree/admin/products/edit_tax_cloud_tic',
+  original: 'a6d7d1941bde020c34025a78466febd8453cf71a'
 )
